03d716751bc4e855b1d5bf14ef76098ea9ad0efb
a-sporez <adornedspore@gmail.com>
2025-07-28 19:48:19 -0400

    blessed testing

--- underscore/FSM/main.py
+++ underscore/FSM/main.py
@@ -1,9 +1,10 @@
-# main endpoint for testing
-# I guessed the module import, still not familiar with python file structure.
+from blessed import Terminal
 from fsm import FSM
 from states.intro import IntroState
 from states.menu import MenuState
 from states.running import RunningState
+
+term = Terminal()
 
 def main():
     # Register states and load intro.
@@ -11,21 +12,17 @@ def main():
     fsm.register('intro', IntroState())
     fsm.register('menu', MenuState())
     fsm.register('running', RunningState())
-
-    fsm.change('intro')
+    fsm.change('menu')
 
     while True:
         fsm.render()
-        user_input = input('> ')
-        if user_input == 'quit':
-            break
-        next_state = fsm.handle_input(user_input)
-        print(f"[debug-main] handle_input() returns: {next_state}")
-        
-        if isinstance(next_state, str):
-            if next_state == 'quit':
+        user_input = term.inkey()
+        result = fsm.handle_input(user_input.name or user_input)
+
+        if isinstance(result, str):
+            if result == 'quit':
                 break
-            fsm.change(next_state)
+            fsm.change(result)
         fsm.update()
 
 if __name__ == '__main__':
--- underscore/FSM/states/menu.py
+++ underscore/FSM/states/menu.py
@@ -1,17 +1,37 @@
 from fsm import State
+from blessed import Terminal
+
+term = Terminal()
 
 class MenuState(State):
+    def __init__(self):
+        self.options = ["Start Game", "Options", "Quit"]
+        self.index = 0
+
     def enter(self, **kwargs):
-        print("Menu Loaded")
+        print(term.clear)
+        print(term.bold_underline("Main Menu"))
 
     def handle_input(self, user_input):
-        if user_input == "start":
-            return 'running'
-        elif user_input == "quit":
-            return 'quit'
+        if user_input in ['KEY_DOWN', 's']:
+            self.index = (self.index + 1) % len(self.options)
+        elif user_input in ['KEY_UP', 'w']:
+            self.index = (self.index + 1) % len(self.options)
+        elif user_input in ['\n', 'KEY_ENTER', 'enter']:
+            selected = self.options[self.index]
+            if selected == "Start Game":
+                return 'running'
+            elif selected == "Quit":
+                return 'quit'
         
     def update(self):
         pass
 
     def render(self):
-        print("[menu] Type 'start' or 'quit'")
+        print(term.clear)
+        print(term.bold_underline("Main Menu"))
+        for i, option in enumerate(self.options):
+            if i == self.index:
+                print(term.reverse(f'> {option}'))
+            else:
+                print(f' {option}')
